d1f159474ac45696b8bd694f067edf85a9f85602
harmolipi <nikob381@gmail.com>
2021-10-06 13:23:01 -0400

    Added #show route and method

--- app/controllers/v1/calendars_controller.rb
+++ app/controllers/v1/calendars_controller.rb
@@ -1,6 +1,29 @@
 class V1::CalendarsController < ApplicationController
+  before_action :get_dates
+  before_action :parse_dates, only: [:show]
+
+  # GET /v1/calendars
   def index
-    json_dates = File.read('public/calendar_database.json')
-    render json: json_dates, status: :ok
+    render json: @json_dates, status: :ok
+  end
+
+  # GET /v1/calendar/params
+  def show
+    selected_date = @parsed_dates.find { |date| date["month"].to_i == calendar_params[:month].to_i && date["date"].to_i == calendar_params[:date].to_i }
+    render json: selected_date
+  end
+
+  private
+
+  def calendar_params
+    params.permit(:month, :date)
+  end
+
+  def get_dates
+    @json_dates = File.read('public/calendar_database.json')
+  end
+
+  def parse_dates
+    @parsed_dates = JSON.parse(@json_dates)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1, :defaults => {format: :json} do
     resources :calendars, only: [:index]
+    resource :calendar, only: [:show]
   end
 end
