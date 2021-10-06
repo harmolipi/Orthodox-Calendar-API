834d6f6a8a1e0eb8eebc8879b3d4386789b1a812
harmolipi <nikob381@gmail.com>
2021-10-06 11:52:02 -0400

    Added versioning

--- app/controllers/v1/calendars_controller.rb
+++ app/controllers/v1/calendars_controller.rb
@@ -1,4 +1,4 @@
-class CalendarsController < ApplicationController
+class V1::CalendarsController < ApplicationController
   def index
     json_dates = File.read('public/calendar_database.json')
     render json: json_dates, status: :ok
