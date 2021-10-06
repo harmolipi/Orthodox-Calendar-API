d64ed261b2c111275148b999e706e11bf92fb202
harmolipi <nikob381@gmail.com>
2021-10-06 16:56:30 -0400

    Added root path to calendars#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   scope module: :v1, defaults: {format: :json}, constraints: ApiConstraints.new(version: 1, default: true) do
     resources :calendars, only: [:index]
     resource :calendar, only: [:show]
+    root 'calendars#index'
   end
 end
